0633bfd07fdd587bf17af239e201e261fb12178f
Kombath-S <155440031+Kombath-S@users.noreply.github.com>
2024-02-14 21:13:12 +0000

    initialize

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,tsx}"],
+  theme: {
+    extend: {
+    },
+    colors: {
+      "gray": {
+        100: "#d8d8d8",
+        200: "#999999",
+        300: "#707070",
+        400: "#242424"
+      },
+      "red": "#ff3d55",
+      "blue": "#0856a2",
+      "pink": "#ff00cc",
+      "transparent": "transparent",
+      "white": "#ffffff"
+    }
+  },
+  plugins: [],
+}
+
